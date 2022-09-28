b463a59f5ef15085376b785b33e67dc07e11a5f2
SoyAnderEscobar <soyanderescobar@gmail.com>
2022-09-28 06:16:12 -0400

    ajuste de estilos

--- src/TodoCount.js
+++ src/TodoCount.js
@@ -4,7 +4,7 @@ import './TodoCount.css'
 function TodoCount(){
     return (
         <React.Fragment>
-        <header className="titulo">
+        <header className="card">
             <h1>Tienes 2 de 3 tareas completas</h1>
         </header>
         </React.Fragment>
