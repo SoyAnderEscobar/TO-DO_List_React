3b7aab2e073bcfafb7984bb262f07f9be86c741c
Anderson <andersonescobar147@gmail.com>
2022-11-08 10:42:33 -0400

    Creacion del primer custom react hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,49 +1,96 @@
-  import React from "react";
-  import { AppUi } from "./AppUi";
+import React from "react";
+import { AppUi } from "./AppUi";
 
-  const defaultTodos =[
+// const defaultTodos =[
 
-    {text:"cortar cebolla", completed:false},
-    {text:"cocinar", completed:false },
-    {text:"comer", completed:false },
-    {text:"Dormir", completed:false },
-  ];
+//   {text:"cortar cebolla", completed:false},
+//   {text:"cocinar", completed:false },
+//   {text:"comer", completed:false },
+//   {text:"Dormir", completed:false },
+// ];
+
+function useLocalStorage(itemName, itemValue){
+
+  //consulta si en el local storage el item recibido por argumento
+  let storedItem = localStorage.getItem(itemName)
+
+  //Defino una variable para ser utilizada mas adelante
+  let defaultItem;
+
+  //valido que el storedItem no este vacio
+  if(storedItem){
+
+    //en caso de no estar vacio parseo el contenido y lo guardo en defaultItem
+    defaultItem= JSON.parse(storedItem)
+
+  }else{
+
+    storedItem = localStorage.setItem(itemName,JSON.stringify(itemValue))
+
+    defaultItem = itemValue
+
+  }
+
+  //Todos
+  const [item, changeItem] = React.useState(defaultItem)
 
 
+  const saveItem = (newItem)=>{
 
-  function App(props) {
+    let stringItem= JSON.stringify(newItem)
 
-    //Search
-    const [searchValue, setSearchValue] = React.useState('');
+    localStorage.setItem(itemName, stringItem)
 
-    //Todos
-    const [todos, setTodo] = React.useState(defaultTodos)
+    changeItem(newItem)
 
-    //Completed Todos cantidad
+  }
+
+  return [item, saveItem]
+
+}
+
+function App(props) {
+  
+  /*Estados de la Aplicación*/
+
+  //Custom React Hook para almacenar informacion en Local Storage
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1',[])
+
+  const [patito, savePatito] = useLocalStorage('PATITO_V1','Hola desde el Local Storage')
+  //Estado para el buscador del aplicativo
+  const [searchValue, setSearchValue] = React.useState('');
+
+
+  /*Variables y constantes de la aplicacion*/
+
+    //Cantidad de todos los todos que estan completados
     const TodoCompleted = todos.filter(todo=> !!todo.completed).length
 
-    //Total Tasks cantidad
+    //Cantidad total de todos
     const total = todos.length
 
-
+    //Declaracion de una variable para ser utilizada con el buscador
     let SearchedTodos=[]
 
+  /*funciones de la aplicacion*/
 
+    //valido que el buscador este vacio
     if (!searchValue>=1) {
 
+      //si el buscador esta vacio entonces los se asigna los todos existentes a SearchedTodos
       SearchedTodos = todos
 
     }else{
 
       SearchedTodos = todos.filter(todo => {
 
-        //convierto el texto de los todos en minuscula
+        //convierto el texto de los todos en minuscula y lo declaro en todoText
         const todoText = todo.text.toLocaleLowerCase();
 
-        //convierto el texto en el buscador en minuscula
+        //convierto el texto en el buscador en minuscula y lo declaro en searchText
         let searchText = searchValue.toLocaleLowerCase();
 
-        //retorno los todos
+        //retorno los todos que tengan alguna similitud con lo escrito en el buscador
         return todoText.includes(searchText)
 
       });
@@ -52,7 +99,7 @@
 
     const completeTodo = (text) => {
 
-      // console.log(`la tarea ${text} fue completada`)
+      // console.log(`la tarea ${text} fue completada`) //esto puede ser una buena manera para ver lo que sucede
 
       const todoKey = todos.findIndex(todo=> todo.text === text )
 
@@ -60,7 +107,7 @@
 
       NewTodo[todoKey].completed=true
 
-      setTodo(NewTodo)
+      saveTodos(NewTodo)
 
     }
 
@@ -72,23 +119,24 @@
 
       NewTodo.splice(todoKey,1)
 
-      setTodo(NewTodo)
+      saveTodos(NewTodo)
 
     }
 
-    return (
-      <AppUi
-        TodoCompleted={TodoCompleted}
-        total={total}
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        SearchedTodos={SearchedTodos}
-        completeTodo={completeTodo}
-        deleteTodo={deleteTodo}
-        nombre={props.nombre}
-      />
-    );
+  return [
+    <p>{patito}</p>,
+    <AppUi
+      TodoCompleted={TodoCompleted}
+      total={total}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      SearchedTodos={SearchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+      nombre={props.nombre}
+    />
+  ];
 
-  }
+}
 
   export default App;
